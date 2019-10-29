057d071dbb2d8c47900003f6d1737eba599b780d
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-29 17:14:04 -0400

    Initial Commit

---
+++ M02/Assignment/app/src/main/java/com/lambda/crash_assignment_patrick_chow/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lambda.crash_assignment_patrick_chow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
